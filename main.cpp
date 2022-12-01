b2479510ae7e049597716c871f76927aa92976fb
Mattia-Scaffidi <116498876+Mattia-Scaffidi@users.noreply.github.com>
2022-12-01 12:14:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+using namespace std;
+bool funzione (int n) {
+    int i=1,x;
 
-int main(){
-  return 0;
+    if(n>1)
+        do{
+            i++;
+            x=n%i;
+        }while(x);
+    if(i==n){
+
+        return true;
+
+
+    }
+    else{
+        return false;
+    }
+
+}
+
+int main() {
+int n ;
+cin>> n;
+bool risultato= funzione(n);
+if (risultato == true){
+    cout<<"il numero e' primo"<<endl;
+}
+else
+{
+    cout<<"il numero non e' primo"<<endl;
+}
+
+
+return 0;
 }
